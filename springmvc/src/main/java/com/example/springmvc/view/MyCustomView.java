cfe0cac561b909182c5edae322629cf715dab87a
No BongJun <sewd1224@gmail.com>
2025-02-03 18:06:10 +0900

    refactor: 사용자 정의 뷰 한글깨짐 현상 해결
    * UTF-8 명시적으로 설정 및 메타태그 추가

--- springmvc/src/main/java/com/example/springmvc/view/MyCustomView.java
+++ springmvc/src/main/java/com/example/springmvc/view/MyCustomView.java
@@ -16,11 +16,17 @@ public class MyCustomView implements View {
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setContentType(getContentType());
+
+        response.setCharacterEncoding("UTF-8"); // 명시적으로 UTF-8 설정
+
         PrintWriter out = response.getWriter();
-        out.println("<html><body>");
+        out.println("<html>");
+        out.println("<head><meta charset='UTF-8'></head>"); // HTML 메타 태그 추가
+        out.println("<body>");
         out.println("<h1>사용자 정의 뷰!!</h1>");
         out.println("<p>나는 사용자 정의 뷰입니다.^^</p>");
-        out.print("</body></html>");
+        out.println("</body>");
+        out.println("</html>");
         out.close();
     }
 }
