5930bc7f3c82c90a11342c535643b01ebfbaba3b
No BongJun <sewd1224@gmail.com>
2025-02-12 14:03:42 +0900

    refactor: JPA의 데이터 삽입 실습

--- jpaexam/src/main/java/org/example/jpa/UserRun.java
+++ jpaexam/src/main/java/org/example/jpa/UserRun.java
@@ -2,6 +2,7 @@ package org.example.jpa;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
 public class UserRun {
@@ -10,5 +11,25 @@ public class UserRun {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         System.out.println(entityManager);
+
+
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        // <<INSERT>> : 데이터 삽입
+        // 엔티티
+        User user = new User();
+//        user.setId(2L);
+        user.setName("sample");
+        user.setEmail("sample@exam.com");
+        // 엔티티가 생성 - 아직 영속성 컨텍스트와는 관계없는 상태 (비영속상태)
+
+        entityManager.persist(user);
+
+        System.out.println("트랜잭션.commit() 실행전");
+        transaction.commit();
+        System.out.println("트랜잭션.commit() 실행후" );
+
+
     }
 }
