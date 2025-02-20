5de9e0abbb203fb3401fac68c4d80c47b842d541
No BongJun <sewd1224@gmail.com>
2025-02-20 16:53:54 +0900

    update: 특정 사원번호의 사원 조회

--- springdatajpa/src/main/java/com/example/springdatajpa4/Application.java
+++ springdatajpa/src/main/java/com/example/springdatajpa4/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 @Slf4j
@@ -18,12 +18,21 @@ public class Application {
     @Bean
     public CommandLineRunner run(EmployeeRepository employeeRepository){
         return args -> {
-            List<Employee> findLastName = employeeRepository.findByLastName("Patel");
-            findLastName.forEach(employee -> log.info("[마지막 이름이 Patel]인 고객 정보 :: {} {} 님", employee.getFirstName(), employee.getLastName()));
+//            List<Employee> findLastName = employeeRepository.findByLastName("Patel");
+//            findLastName.forEach(employee -> log.info("[마지막 이름이 Patel]인 사원 정보 :: {} {} 님", employee.getFirstName(), employee.getLastName()));
+//
+//            // 1. 연봉 12000이상 조회
+//            List<Employee> findMoreSalary = employeeRepository.findBySalaryGreaterThanEqual(12000d);
+//            findMoreSalary.forEach(employee -> log.info("[연봉 12000 이상]인 사원 : {}의 연봉 [{}]", employee.getFirstName(), employee.getSalary()));
 
-            // JPA 실습
-            List<Employee> findMoreSalary = employeeRepository.findBySalaryGreaterThanEqual(12000d);
-            findMoreSalary.forEach(employee -> log.info("[연봉 12000 이상]인 고객 : {}의 연봉 [{}]", employee.getFirstName(), employee.getSalary()));
+            // 2. 사원 번호로 조회
+            Optional<Employee> findEmpId = employeeRepository.findById(176);
+            if(findEmpId.isPresent()){
+                Employee employee = findEmpId.get();
+                log.info("[사원번호 176]인 사원 : {}", employee.getLastName());
+            }
+
+            
         };
     }
 }
