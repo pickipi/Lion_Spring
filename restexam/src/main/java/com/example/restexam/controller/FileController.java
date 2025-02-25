3e61d62fe172a9794f92b94455fdd96041705c71
No BongJun <sewd1224@gmail.com>
2025-02-25 15:02:02 +0900

    update: 파일 업로드 실습 (@RestController)

--- restexam/src/main/java/com/example/restexam/controller/FileController.java
+++ restexam/src/main/java/com/example/restexam/controller/FileController.java
@@ -1,11 +1,17 @@
 package com.example.restexam.controller;
 
+import com.example.restexam.domain.UploadInfo;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -25,6 +31,23 @@ public class FileController {
             response.flushBuffer();
         }catch(IOException e){
             log.error("파일 다운로드 중 오류 발생 : " + e.getMessage());
+        }
+    }
+
+    // 파일 업로드
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam(name = "info", required = false) UploadInfo uploadInfo){
+        log.info(file.getOriginalFilename());
+
+        try(InputStream inputStream = file.getInputStream()){
+            StreamUtils.copy(inputStream, new FileOutputStream("c:/Temp/DumpFile/upload/" + file.getOriginalFilename()));
+            return ResponseEntity.ok().body("파일저장이 완료되었습니다." + file.getOriginalFilename());
+        }catch(IOException e){
+            return ResponseEntity
+                    .badRequest()
+                    .body("파일 업로드 실패 : " + file.getOriginalFilename());
         }
     }
 }
