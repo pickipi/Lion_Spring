bfa08a9560dc64e302ec1da75f2cf406b3a6ae32
No BongJun <sewd1224@gmail.com>
2025-02-26 11:28:56 +0900

    feat: 실습 - Product의 DTO 생성

--- restexam/src/main/java/com/example/restexam/domain/Product.java
+++ restexam/src/main/java/com/example/restexam/domain/Product.java
@@ -1,17 +1,20 @@
 package com.example.restexam.domain;
 
 
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter@Setter@NoArgsConstructor
 @Table(name = "products")
+@Entity
+@AllArgsConstructor // DTO를 통해 값을 복사하기 위해선 전체 필드를 가져오는 생성자도 필요
 public class Product {
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private double price;
-
-
 }
