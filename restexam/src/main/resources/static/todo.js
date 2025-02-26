13fb280b95ffb8a894fb793145e711ad76abeea8
No BongJun <sewd1224@gmail.com>
2025-02-26 14:52:53 +0900

    feat: products 프로젝트 스타일 적용
    * todo 프로젝트 오류로 인한 스타일 재추가

--- restexam/src/main/resources/static/todo.js
+++ restexam/src/main/resources/static/todo.js
@@ -4,48 +4,46 @@ let basicDatas = [];
 
 function getTodos(){
     const xhr = new XMLHttpRequest();
-    xhr.open("get","http://localhost:8080/api/todos");
+    xhr.open("get","http://localhost/api/todos");
     xhr.onreadystatechange = function(){
        if(xhr.readyState === 4 ){
            if(xhr.status === 200){
                // json 문자열을 json 객체로 변환시킨다.
                let todos = JSON.parse(this.responseText);
-                console.log(todos);
+               // console.log(todos);
                for(let i = 0; i < todos.length; i++){
                    todoItemAdd(todos[i]);
                }
             }
         }
    }
-   xhr.send();
+   xhr.send();          
 }
 
 function updateTodo(id){
-//    let updateTodo = {"id":id};
+    let updateTodo = {"id":id};
     let xhr = new XMLHttpRequest();
-    xhr.open('PATCH','http://localhost:8080/api/todos/'+id);
+    xhr.open('PATCH','http://localhost/api/todos/'+id);
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     // xhr.send(JSON.stringify(updateTodo));
     xhr.send();
 }
 
 function deleteTodo(id){
-//    let delTodo = {"id": id};
+    let delTodo = {"id": id};
     let xhr = new XMLHttpRequest();
-//    xhr.open('DELETE','http://localhost:8080/api/todos');
-    xhr.open('DELETE','http://localhost:8080/api/todos/'+id);
+    xhr.open('DELETE','http://localhost/api/todos');
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-     xhr.send(JSON.stringify());
-//    xhr.send(JSON.stringify(delTodo));
+    xhr.send(JSON.stringify(delTodo));
 }
 
 function postTodo(todo){
     let xhr = new XMLHttpRequest();
-    xhr.open('post','http://localhost:8080/api/todos');
+    xhr.open('post','http://localhost/api/todos');
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhr.onload = function(){
         // 글쓰기를 할 경우 ajax의 결과를 json 오브젝트로 변환하여 추가한다.
-        todoItemAdd(JSON.parse(xhr.responseText));
+        todoItemAdd(JSON.parse(xhr.responseText));   
     };
     xhr.send(JSON.stringify({"todo":todo}));
 }
@@ -55,12 +53,12 @@ getTodos();
 let todoUl = document.querySelector("#todo-item-list");
 
 todoUl.addEventListener('click',function(event){
-    let eventTarget = event.target;
+    let eventTarget = event.target; 
     if(event.target.tagName === 'LI' || event.target.tagName === 'SPAN'){
         if(event.target.tagName ==='LI'){
             eventTarget =eventTarget.querySelector('.todo-text');
         }
-
+       
         let liObj = eventTarget.parentElement;
 
         updateTodo(liObj.getAttribute("id"));
@@ -70,25 +68,25 @@ todoUl.addEventListener('click',function(event){
 
  const inputbtn = document.querySelector('.add-button');
 //  const myinput = document.querySelector('#myInput');
-
+ 
 
 inputbtn.addEventListener('click', function(){
     inputValue = document.querySelector('#myInput').value;
     if(inputValue === ''){
-        alert("할 일을 입력해 주세요^^ ");
+        alert("할 일을 입력해 주세요^^ "); 
         return;
     }
     document.querySelector('#myInput').value = '';
     postTodo(inputValue);
 });
 
-function todoItemAdd(todoObj){
+function todoItemAdd(todoObj){   
     console.log(todoObj);
     const li = document.createElement('li');
     li.className = "todo-item";
     const textSpan = document.createElement('SPAN');
     textSpan.className = "todo-text" ;
-
+    
     const todotxt = document.createTextNode(todoObj.todo);
     textSpan.appendChild(todotxt);
     if(todoObj.done == true){
@@ -98,7 +96,7 @@ function todoItemAdd(todoObj){
     li.appendChild(textSpan);
     li.setAttribute("id", todoObj.id);
     document.getElementById('todo-item-list').appendChild(li);
-
+    
     const removeSpan = document.createElement("SPAN");
     const removetxt = document.createTextNode("\u00D7");
     removeSpan.className = "remove";
@@ -107,14 +105,12 @@ function todoItemAdd(todoObj){
     li.appendChild(removeSpan);
 
     // 동적으로 x버튼을 클릭했을 때 처리해야할 이벤트를 추가한다.
-    removeSpan.addEventListener('click',function(){
-        event.stopPropagation(); // 이벤트 전파 (=이벤트 버블링) 가능성 방지 -> (X)버튼 클릭 시 updateTodo()가 실행되지 않도록 수정
-        let liObj = this.parentElement;
-        console.log(liObj);
-        deleteTodo(liObj.getAttribute("id"));
-        liObj.remove();
-//        return false; // 이벤트 전파 (=이벤트 버블링)가능성 존재
-// return false를 하지 않으면 수정이 되면서 update를 호출한다.이미 삭제된 것을 수정하려고 하니 오류가 발생한다.
-
-    });
+        removeSpan.addEventListener('click',function(){
+            event.stopPropagation(); // 이벤트 전파 (=이벤트 버블링) 가능성 방지 -> (X)버튼 클릭 시 updateTodo()가 실행되지 않도록 수정
+            let liObj = this.parentElement;
+            console.log(liObj);
+            deleteTodo(liObj.getAttribute("id"));
+            liObj.remove();
+    //        return false; // 이벤트 전파 (=이벤트 버블링)가능성 존재
+    // return false를 하지 않으면 수정이 되면서 update를 호출한다.이미 삭제된 것을 수정하려고 하니 오류가 발생한다.
 };
