8c644f572ffc5e136c1a8dddf756a3bc5a175370
No BongJun <sewd1224@gmail.com>
2025-02-27 13:00:32 +0900

    feat: ThreadLocal 실습 - UserContext

---
+++ securityexam/src/main/java/com/example/beforesecurity/User.java
@@ -0,0 +1,11 @@
+package com.example.beforesecurity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter@Setter
+@AllArgsConstructor
+public class User {
+    private String name;
+}
