b04f856b6f923c65203aaba7b617455ecaa63a83
No BongJun <sewd1224@gmail.com>
2025-03-04 13:08:56 +0900

    feat: Security + DB - Service, Config

---
+++ securityexam/src/main/java/com/example/securityexam4/service/UserService.java
@@ -0,0 +1,36 @@
+package com.example.securityexam4.service;
+
+import com.example.securityexam4.domain.Role;
+import com.example.securityexam4.domain.User;
+import com.example.securityexam4.repository.RoleRepository;
+import com.example.securityexam4.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    // - 회원가입
+    @Transactional
+    public User registUser(User user){
+        // Role정보를 User엔티티에 채워줌
+        // 회원가입 요청 시 User 권한으로 가입 (roleRepository 활용)
+        Role userRole = roleRepository.findByName("USER").get();
+        user.setRoles(Collections.singleton(userRole));
+
+        // 패스워드 : 반드시 인코딩(=암호화)되어야함 (PasswordEncoder 활용)
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        return userRepository.save(user);
+    }
+}
