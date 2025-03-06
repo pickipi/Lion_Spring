72e9172606a8e66097931b15e5955c272d45fd69
No BongJun <sewd1224@gmail.com>
2025-03-06 10:45:49 +0900

    update: Create Test - Refresh Token

--- securityexam/src/main/java/com/example/jwtexam/jwt/util/JwtTokenizerMain.java
+++ securityexam/src/main/java/com/example/jwtexam/jwt/util/JwtTokenizerMain.java
@@ -1,7 +1,9 @@
 package com.example.jwtexam.jwt.util;
 
+import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 @Slf4j
@@ -11,8 +13,24 @@ public class JwtTokenizerMain {
                 "12345678901234567890123456789012",
                 "12345678901234567890123456789012");
 
+        // Access Token 생성
         String accessToken = jwtTokenizer.createAccessToken(1L, "test@test.com", "test", "testuser", Arrays.asList("ROLE_USER"));
-
         log.info("[ACCESS TOKEN] : {}", accessToken);
+
+        // Refresh Token 생성
+        String refreshToken = jwtTokenizer.createRefreshToken(1L, "test@test.com", "test", "testuser", Arrays.asList("ROLE_USER"));
+        log.info("[REFRESH TOKEN] : {}", refreshToken);
+
+        // parseToken 테스트
+        byte[] secretKey = "12345678901234567890123456789012".getBytes(StandardCharsets.UTF_8);
+
+        Claims claims = jwtTokenizer.parseToken(accessToken, secretKey);
+        log.info(claims.getSubject());
+        log.info((String) claims.get("username"));
+
+        // Bearer 테스트
+        accessToken = "Bearer " + accessToken;
+        Long userIdFromToken = jwtTokenizer.getUserIdFromToken(accessToken);
+        log.info(String.valueOf(userIdFromToken));
     }
 }
