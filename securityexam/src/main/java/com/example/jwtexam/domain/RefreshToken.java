70630b5ac96172eeac0a1f0a6a1ed7c96e2f6315
No BongJun <sewd1224@gmail.com>
2025-03-06 11:11:31 +0900

    feat: Defining Refresh Token class

---
+++ securityexam/src/main/java/com/example/jwtexam/domain/RefreshToken.java
@@ -0,0 +1,17 @@
+package com.example.jwtexam.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "refresh_token")
+@Getter@Setter
+public class RefreshToken{
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+    private String value;
+}
