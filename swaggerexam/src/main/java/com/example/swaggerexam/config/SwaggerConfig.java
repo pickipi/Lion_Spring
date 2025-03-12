2b8cbd20aad12d8b1b81cceeef70ff111de4e429
No BongJun <sewd1224@gmail.com>
2025-03-12 16:39:43 +0900

    update: Add SecurityFilterChain, validation

--- swaggerexam/src/main/java/com/example/swaggerexam/config/SwaggerConfig.java
+++ swaggerexam/src/main/java/com/example/swaggerexam/config/SwaggerConfig.java
@@ -7,9 +7,12 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @OpenAPIDefinition(
@@ -27,6 +30,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 )
 @EnableWebSecurity
 public class SwaggerConfig {
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http
+                        .csrf(csrf -> csrf.disable())
+                        .sessionManagement(sessionManagement ->
+                                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        )
+                        .authorizeHttpRequests(authorizeRequests ->
+                                authorizeRequests
+                                        .requestMatchers("/auth/register", "/auth/login").permitAll()
+                                        .anyRequest().authenticated()
+                        );
+                return http.build();
+        }
+
         @Bean
         public PasswordEncoder passwordEncoder(){
                 return new BCryptPasswordEncoder();
